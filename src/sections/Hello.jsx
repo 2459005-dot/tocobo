b645b19d9e8c6fcdc9b4f34ef02c1d4468289537
2459005-dot <2459005@kbu.ac.kr>
2025-08-28 17:11:54 +0900

    .

--- src/sections/Hello.jsx
+++ src/sections/Hello.jsx
@@ -4,15 +4,15 @@ import { helloData } from '../util/hello'
 
 const Hello = () => {
     return (
-        <div className='inner'>
+        <div className='inner hello-inner'>
             <div className="img-wrap">
-                <img src="#" alt="" />
+                <img src={helloData.media.src} alt={helloData.media.alt} />
             </div>
             <div className="t-wrap">
                 <p className="txt-2">{helloData.eyebrow}</p>
                 <h2 className="tit">{helloData.title}</h2>
                 <p className="txt-1">{helloData.description}</p>
-                <a href="" className='btn'>{helloData.cta.label}</a>
+                <a href={helloData.cta.href} className='btn'>{helloData.cta.label}</a>
             </div>
         </div>
     )
